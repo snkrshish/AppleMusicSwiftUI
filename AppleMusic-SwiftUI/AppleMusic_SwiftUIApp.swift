bc868af56d4e83856ca22e91db25beb8c3817095
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2024-01-29 14:22:18 +0800

    Create project

---
+++ AppleMusic-SwiftUI/AppleMusic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SwiftUIApp.swift
+//  AppleMusic-SwiftUI
+//
+//  Created by Никита Шиляев on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
